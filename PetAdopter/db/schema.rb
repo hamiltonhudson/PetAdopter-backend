7b5a4fe9bf12c03f5cdd9e60cf55913138d50003
gbellini90 <gbellini90@gmail.com>
2019-01-23 13:00:01 -0500

    add adopted column to pets table

--- PetAdopter/db/schema.rb
+++ PetAdopter/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_220013) do
+ActiveRecord::Schema.define(version: 2019_01_23_175903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_220013) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "adopted", default: false
   end
 
   create_table "users", force: :cascade do |t|
