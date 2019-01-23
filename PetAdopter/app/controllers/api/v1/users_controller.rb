509dfe35a49ff9829ce2fe927ad56b9dd41e836d
Hamilton Hudson <ehamiltonhudson@gmail.com>
2019-01-23 17:17:03 -0500

    fix update method in controller

--- PetAdopter/app/controllers/api/v1/users_controller.rb
+++ PetAdopter/app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,7 @@ end
     if @user.update(user_params)
       render json: @user, status: :ok
     else
-      render json: @users.errors.full_messages,status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
