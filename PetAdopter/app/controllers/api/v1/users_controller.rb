2aa403b1b85e9da6071a79ac413c79284e5a4e86
Hamilton Hudson <ehamiltonhudson@gmail.com>
2019-01-23 17:04:25 -0500

    add update method in users controller

--- PetAdopter/app/controllers/api/v1/users_controller.rb
+++ PetAdopter/app/controllers/api/v1/users_controller.rb
@@ -15,9 +15,22 @@ class Api::V1::UsersController < ApplicationController
   end
 end
 
+  def update
+    @user = User.find(user_params[:id])
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @users.errors.full_messages,status: :unprocessable_entity
+    end
+  end
+
+
+  end
+
+
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :pet_id)
   end
 
 end
