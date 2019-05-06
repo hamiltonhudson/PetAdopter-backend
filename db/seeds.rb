236b58a3de925831e6be5ea8c259b6d9e61d67cb
Hamilton <ehamiltonhudson@gmail.com>
2019-05-05 22:53:15 -0400

    adds more pets to seed data (& updates users info)

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ parsed_data_sfth["petfinder"]["pets"]["pet"].each do |pet|
   else
     breed = pet["breeds"]["breed"]["$t"]
   end
-    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+    photo = pet["media"]["photos"]["photo"].find{ |photo| photo["@size"] == "x" }["$t"]
   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
 end
 
@@ -45,8 +45,23 @@ parsed_data_bideawee["petfinder"]["pets"]["pet"].each do |pet|
     breed = pet["breeds"]["breed"]["$t"]
   end
 
-    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+    photo = pet["media"]["photos"]["photo"].find{ |photo| photo["@size"] == "x" }["$t"]
   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
+end
+
+#Waggytail Rescue data
+waggytail = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY447&count=100&format=json")
+parsed_data_waggytail = JSON.parse(waggytail)
+
+parsed_data_waggytail["petfinder"]["pets"]["pet"].each do |pet|
+  next if !pet["media"]["photos"]
+  if pet["breeds"]["breed"][0]
+    breed = pet["breeds"]["breed"][0]["$t"]
+  else
+    breed = pet["breeds"]["breed"]["$t"]
+  end
+    photo = pet["media"]["photos"]["photo"].find{ |photo| photo["@size"] == "x" }["$t"]
+  Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"], photo: photo)
 end
 
 # Animal care Center of NYC data <-- changed to:
@@ -61,16 +76,41 @@ parsed_data_animalCC["petfinder"]["pets"]["pet"].each do |pet|
   else
     breed = pet["breeds"]["breed"]["$t"]
   end
-    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"]=="x"}["$t"]
+    photo = pet["media"]["photos"]["photo"].find { |photo| photo["@size"] == "x" }["$t"]
   Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId:pet["shelterId"]["$t"],photo:photo)
 end
 
-# User.create!(name: "Gabby", username: "gabllini", password: "hellur")
-# User.create!(name: "Hamilton", username: "hambam", password: "howdy")
-# User.create!(name: "Athena", username: "theeny", password: "beans")
+#GiveMeShelter data
+givemeshelter = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY1113&count=100&format=json")
+parsed_data_gms = JSON.parse(givemeshelter)
+
+parsed_data_gms["petfinder"]["pets"]["pet"].each do |pet|
+  next if !pet["media"]["photos"]
+  if pet["breeds"]["breed"][0]
+    breed = pet["breeds"]["breed"][0]["$t"]
+  else
+    breed = pet["breeds"]["breed"]["$t"]
+  end
+    photo = pet["media"]["photos"]["photo"].find{ |photo| photo["@size"] == "x" }["$t"]
+  Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId: pet["shelterId"]["$t"], photo: photo)
+end
 
 
+#NewBeginning Animal Rescue data
+  newbeginning = RestClient.get("http://api.petfinder.com/shelter.getPets?key=#{API_KEY}&id=NY1121&count=100&format=json")
+  parsed_data_nb = JSON.parse(newbeginning)
 
-User.create!(name: "Gabby", username: "gbellini90@gmail.com", password:"hi" )
-User.create(name: "Hamilton", username: "hambam@email.com",password:"hi")
-User.create(name:"HamGab", username:"hamgab@gmail.com",password:"hi")
+  parsed_data_nb["petfinder"]["pets"]["pet"].each do |pet|
+    next if !pet["media"]["photos"]
+    if pet["breeds"]["breed"][0]
+      breed = pet["breeds"]["breed"][0]["$t"]
+    else
+      breed = pet["breeds"]["breed"]["$t"]
+    end
+      photo = pet["media"]["photos"]["photo"].find{ |photo| photo["@size"] == "x" }["$t"]
+    Pet.create!(name: pet["name"]["$t"], age: pet["age"]["$t"], size: pet["size"]["$t"], sex: pet["sex"]["$t"], description: pet["description"]["$t"], animal: pet["animal"]["$t"], breed: breed, shelterId: pet["shelterId"]["$t"], photo:photo)
+  end
+
+User.create!(name: "Gabby", username: "gabllini", password: "hellur")
+User.create!(name: "Hamilton", username: "hambam", password: "howdy")
+User.create!(name: "Athena", username: "theeny", password: "beans")
